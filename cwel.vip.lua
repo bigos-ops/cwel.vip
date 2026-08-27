137b036b8a22f8050edc94b8cdde76b84b4b39c7
bigos-ops <diggern422@gmail.com>
2026-08-27 16:16:02 +0200

    Update cwel.vip.lua

--- cwel.vip.lua
+++ cwel.vip.lua
@@ -1307,7 +1307,7 @@ do
 
             local State = KeyPicker:GetState();
 
-            ContainerLabel.Text = string.format('[%s] %s (%s)', KeyPicker.Value, Info.Text, KeyPicker.Mode);
+            ContainerLabel.Text = string.format('[%s] %s (%s)', KeyPicker.Value, Info.Text, KeyPicker.Mode or 'Always');
 
             ContainerLabel.Visible = true;
             ContainerLabel.TextColor3 = State and Library.AccentColor or Library.FontColor;
